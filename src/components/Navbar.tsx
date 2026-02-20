43ac0bfa7d7661ccc320e113fd45f0ecc2c3ab65
Rashid-C <anurashid105@gmail.com>
2026-02-20 22:20:54 +0400

    e-commerce-ui

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import SearchBar from './SearchBar'
+import { Bell, Home, ShoppingCart } from 'lucide-react'
+
+function Navbar() {
+  return (
+    <nav className='w-full flex items-center justify-between border-b border-gray-200 pb-4'>
+
+
+      {/* left */}
+      <Link href="/" className='flex items-center '>
+        <Image src="/logo.png" className='w-12 h-12 md:h-9 ' width={36} height={36} alt="AZC Logo" />
+        <p  className=' hidden md:blocktext-md font-medium tracking-wider'>AZC</p>
+      </Link>
+      {/* right */}
+      <div className='flex items-center gap-6'>
+        <SearchBar />
+        <Link href="/">
+        <Home  className='w-4 h-4 text-gray-600'/>
+        </Link>
+        <Bell className='w-4 h-4 text-gray-600'/>
+        <ShoppingCart className='w-4 h-4 text-gray-600'/>
+        <Link href="/login">Sign in</Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
