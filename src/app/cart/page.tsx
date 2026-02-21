58093943e9a88e5830fbfad92d868610f7584578
Rashid-C <anurashid105@gmail.com>
2026-02-21 14:31:14 +0400

    shipping data type

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,11 @@
 "use client";
+import PaymentForm from "@/components/PaymentForm";
+import ShippingForm from "@/components/ShippingForm";
 import { CartItemsType } from "@/types";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Trash2 } from "lucide-react";
+import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 const steps = [
     {
         id: 1,
@@ -65,6 +68,7 @@ const cartItems: CartItemsType = [
 const CartPage = () => {
     const searchParams = useSearchParams();
     const router = useRouter();
+    const [shippingForm, setShippingForm] = useState(null);
 
     const activeStep = parseInt(searchParams.get("step") || "1");
 
@@ -96,16 +100,63 @@ const CartPage = () => {
             <div className="w-full flex flex-col lg:flex-row gap-16">
                 {/* step */}.
                 <div className="w-full lg:w-7/12 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8">
-                    1
+                    {activeStep === 1 ? (
+                        cartItems.map((item) => (
+                            // single cart item
+                            <div className="flex items-center justify-between" key={item.id}>
+                                {/*image and details  */}
+                                <div className="flex gap-8">
+                                    {/* image */}
+                                    <div className="relative w-32 h-32 bg-gray-50 rounded-lg overflow-hidden">
+                                        <Image
+                                            src={item.images[item.selectedColor]}
+                                            alt={item.name}
+                                            fill
+                                            className="object-contain"
+                                        />
+                                    </div>
+                                    {/*item details  */}
+                                    <div className="flex flex-col justify-between">
+                                        <div className="flex flex-col gap-1">
+                                            <p className="text-sm font-medium ">{item.name}</p>
+                                            <p className="text-xs text-gray-500">
+                                                Quantity: {item.quantity}
+                                            </p>
+                                            <p className="text-xs text-gray-500">
+                                                Size: {item.selectedSize}
+                                            </p>
+                                            <p className="text-xs text-gray-500">
+                                                Color: {item.selectedColor}
+                                            </p>
+                                        </div>
+                                        <p className="font-medium">${item.price.toFixed(2)}</p>
+                                    </div>
+                                </div>
+                                {/* delete but */}
+                                <button className="w-8 hover:bg-red-200 transition-all duration-300 h-8 rounded-full bg-red-100 text-red-400 flex items-center justify-center cursor-pointer ">
+                                    <Trash2 className="w-3 h-3" />
+                                </button>
+                            </div>
+                        ))
+                    ) : activeStep === 2 ? (
+                        <ShippingForm />
+                    ) : activeStep === 3 && shippingForm ? (
+                        <PaymentForm />
+                    ) : (
+                        <p className="text-sm text-gray-500">
+                            Please fill in the shipping form to continue.
+                        </p>
+                    )}
                 </div>
                 {/* deatils */}
-                <div className="w-full lg:w-5/12 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8">
+                <div className="w-full lg:w-5/12 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8 h-max">
                     <h2 className="font-semibold">Cart Details</h2>
                     <div className="flex flex-col gap-4">
                         <div className="flex justify-between text-sm">
                             <p className="text-gray-500">Subtotal</p>
                             <p className="font-medium">
-                                $ {cartItems
+                                ${" "}
+                                {cartItems
                                     .reduce((acc, item) => acc + item.price * item.quantity, 0)
                                     .toFixed(2)}
                             </p>
@@ -124,17 +175,23 @@ const CartPage = () => {
 
                         <div className="flex justify-between">
                             <p className="text-gray-800 font-semibold">Total</p>
-                            <p className="font-medium">${cartItems
-                                .reduce((acc, item) => acc + item.price * item.quantity, 0)
-                                .toFixed(2)}</p>
+                            <p className="font-medium">
+                                $
+                                {cartItems
+                                    .reduce((acc, item) => acc + item.price * item.quantity, 0)
+                                    .toFixed(2)}
+                            </p>
                         </div>
-
-
                     </div>
-             {  activeStep===1 &&    ( <button onClick={()=>router.push("/cart?step=2",{scroll:false})} className="w-full hover:bg-gray-900 transition-all duration-300 flex gap-2 items-center justify-center text-white bg-gray-800 p-2 rounded-lg cursor-pointer">
-                        Continue
-                        <ArrowRight className="w-3 h-3" />
-                    </button>)}
+                    {activeStep === 1 && (
+                        <button
+                            onClick={() => router.push("/cart?step=2", { scroll: false })}
+                            className="w-full hover:bg-gray-900 transition-all duration-300 flex gap-2 items-center justify-center text-white bg-gray-800 p-2 rounded-lg cursor-pointer"
+                        >
+                            Continue
+                            <ArrowRight className="w-3 h-3" />
+                        </button>
+                    )}
                 </div>
             </div>
         </div>
--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,36 @@
-export type ProductType={
-    id:string | number;
-    name:string;
-    shortDescription:string;
-    description:string,
-    price:number;
-    sizes:string[];
-    colors:string[];
-    images:Record<string, string>
-}
-export type ProductsType= ProductType[]
+import { z } from "zod";
 
-export type CartItemType=ProductType &{
-   quantity:number;
-   selectedSize:string;
-   selectedColor:string; 
-}
-export type CartItemsType= CartItemType[]
+export type ProductType = {
+    id: string | number;
+    name: string;
+    shortDescription: string;
+    description: string;
+    price: number;
+    sizes: string[];
+    colors: string[];
+    images: Record<string, string>;
+};
+export type ProductsType = ProductType[];
+
+export type CartItemType = ProductType & {
+    quantity: number;
+    selectedSize: string;
+    selectedColor: string;
+};
+export type CartItemsType = CartItemType[];
+
+//1.44
+export const shippingFormSchema = z.object({
+    name: z.string().min(1, "Name is required!"),
+    email: z.string().min(1, "Email is required!"),
+    phone: z
+        .string()
+        .min(7, "Phone number must be between 7 and 10 digits")
+        .max(10, "Phone number must be between 7 and 10 digits")
+        .regex(/^\d+$/, "Phone number must contain only digits"),
+    address: z.string().min(1, "Address is required!"),
+    city: z.string().min(1, "City is required!"),
+
+});
+
+export type ShippingFormInputs = z.infer<typeof shippingFormSchema>
